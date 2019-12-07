eef0cd100395580a771d5255f0e853475059e74e
Dinh Hau <nguyendinhhau1807@gmail.com>
2019-12-07 17:55:59 +0700

    Hau:Update database

--- yolo.sql
+++ yolo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th12 04, 2019 lúc 03:06 PM
--- Phiên bản máy phục vụ: 10.4.6-MariaDB
--- Phiên bản PHP: 7.3.9
+-- Host: 127.0.0.1
+-- Generation Time: Dec 07, 2019 at 11:54 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,88 +19,30 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `yolo`
+-- Database: `yolo`
 --
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `posts`
+-- Table structure for table `friendship`
 --
 
-CREATE TABLE `posts` (
-  `id` int(11) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
-  `content` text CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
-  `image` mediumblob DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `displayName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `yearOfBirth` int(11) NOT NULL,
-  `nickName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `introContent` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `backgroundImage` mediumblob NOT NULL,
-  `createdDate` date NOT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `phoneNumber` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `avatarImage` mediumblob DEFAULT NULL,
-  `status` int(11) NOT NULL DEFAULT 0,
-  `code` varchar(16) COLLATE utf8_unicode_ci NOT NULL
+CREATE TABLE `friendship` (
+  `userId1` int(11) NOT NULL,
+  `userId2` int(11) NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Chỉ mục cho các bảng đã đổ
+-- Indexes for dumped tables
 --
 
 --
--- Chỉ mục cho bảng `posts`
+-- Indexes for table `friendship`
 --
-ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`,`userId`),
-  ADD KEY `fk_posts_users` (`userId`);
-
---
--- Chỉ mục cho bảng `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT cho các bảng đã đổ
---
-
---
--- AUTO_INCREMENT cho bảng `posts`
---
-ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
-
---
--- AUTO_INCREMENT cho bảng `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
-
---
--- Các ràng buộc cho các bảng đã đổ
---
-
---
--- Các ràng buộc cho bảng `posts`
---
-ALTER TABLE `posts`
-  ADD CONSTRAINT `fk_posts_users` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+ALTER TABLE `friendship`
+  ADD PRIMARY KEY (`userId1`,`userId2`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
