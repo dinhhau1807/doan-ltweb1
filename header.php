63695e1420cf851a6c0e2db0d643c24c757ade22
nttanh6299 <nttanh6299@gmail.com>
2019-12-14 21:45:45 +0700

    TA: update layout

--- header.php
+++ header.php
@@ -2,58 +2,54 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>LTWEB1 - BTN</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="./assets/css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LTWEB1 - BTN</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
 <body>
-  <header id="header">
-    <!-- Fixed navbar -->
-      <nav class="navbar navbar-expand-md fixed-top bg-success">
-        <div class="container">
-          <a href="./" class="navbar-brand text-light">
-            <h1>Yolo</h1>
-          </a>
-          <div>
-            <?php if ($currentUser) : ?>
-              <div class="d-flex justify-content-center align-items-center ml-auto">
-                <a data-toggle="tooltip"  title="Thông tin cá nhân" href="./profile.php?id=<?php echo $currentUser['id']; ?>" class="btn btn-success d-flex justify-content-center align-items-center">
-                <i style="margin-right: 6px;" class="fa fa-user"></i>  
-                <?php echo $currentUser['displayName']; ?>
+    <header id="header">
+        <!-- Fixed navbar -->
+        <nav class="navbar navbar-expand-md fixed-top bg-success">
+            <div class="container">
+                <a href="./" class="navbar-brand text-light">
+                    <h1 style="font-weight: 700;">Yolo</h1>
                 </a>
-                <span class="divider"></span>
-                <a href="./" class="btn btn-success">Trang chủ</a>
-                <span class="divider"></span>
-                <div class="icon-group">
-                  <a class="icon" href="./friends/request.php" data-toggle="tooltip"  title="Lời mời kết bạn">
-                    <i class="fa fa-users">
-                      <span class="notify"></span>
-                    </i>                    
-                  </a>
-                  <a class="icon" href="./messages.php" data-toggle="tooltip"  title="Tin nhắn">
-                    <i class="fa fa-comment">
-                      <span class="notify"></span>
-                    </i>                    
-                  </a>
-                  <a class="icon" href="./notification.php" data-toggle="tooltip"  title="Thông báo">
-                    <i class="fa fa-bell">
-                      <span class="notify"></span>
-                    </i>                    
-                  </a>
-                  <a class="icon" href="./logout.php" data-toggle="tooltip"  title="Đăng xuất">
-                    <i class="fa fa-sign-out"></i>
-                  </a>
+                <div>
+                    <?php if ($currentUser) : ?>
+                    <div class="d-flex justify-content-center align-items-center ml-auto">
+                        <a data-toggle="tooltip" title="Thông tin cá nhân"
+                            href="./profile.php?id=<?php echo $currentUser['id']; ?>"
+                            class="btn btn-success d-flex justify-content-center align-items-center">
+                            <i style="margin-right: 6px;" class="fa fa-user"></i>
+                            <?php echo $currentUser['displayName']; ?>
+                        </a>
+                        <span class="divider"></span>
+                        <a href="./" class="btn btn-success">Trang chủ</a>
+                        <span class="divider"></span>
+                        <div class="icon-group">
+                            <a class="icon" data-toggle="tooltip" href="./friend-request.php" title="Lời mời kết bạn">
+                                <i class="fa fa-users">
+                                </i>
+                            </a>
+                            <a class="icon" data-toggle="tooltip" href="./messages.php" title="Tin nhắn">
+                                <i class="fa fa-comment">
+                                </i>
+                            </a>
+                            <a class="icon" href="./logout.php" data-toggle="tooltip" title="Đăng xuất">
+                                <i class="fa fa-sign-out"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <?php endif; ?>
                 </div>
-              </div>
-            <?php endif; ?>
-          </div>
-        </div>
-      </nav>
-  </header>
-  
-  <main role="main" class="container pb-2">
+            </div>
+        </nav>
+    </header>
+
+    <main role="main" class="container pb-2">
--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $page = 'index';
 ?>
 <?php include 'header.php' ?>
 <?php if ($currentUser) : ?>
-  <?php
+<?php
     $newFeeds = findAllPosts();
     $success = true;
     if (isset($_POST['content'])) {
@@ -23,86 +23,163 @@ $page = 'index';
       }
     }
     ?>
-    <div class="inner">
-  <?php if (!$success) : ?>
+<div class="inner">
+    <?php if (!$success) : ?>
     <div class="alert alert-danger" role="alert">
-      Nội dung không được rỗng và dài quá 1024 ký tự!
+        Nội dung không được rỗng và dài quá 1024 ký tự!
     </div>
-  <?php endif; ?>
-  <form method="POST" enctype="multipart/form-data">
-    <div class="form-group">
-      <textarea class="form-control" id="content" name="content" rows="3" placeholder="<?php echo $currentUser['displayName'] ?> ơi, bạn đang nghĩ gì vậy?"></textarea>
-    </div>
-    <div class="d-flex justify-content-between align-items-center">
-    <div class="upload-btn-wrapper">
-      <button class="btn">🖼️ <strong>Ảnh/Video</strong></button>
-      <input type="file" id="postImage" name="postImage" />
-    </div>
-    <div class="select-privacy" style="width:260px;">
-      <select>
-        <option value="1">Công khai</option>
-        <option value="2">Bạn bè</option>
-        <option value="3">Chỉ mình tôi</option>
-      </select>
-    </div>
-    <button type="submit" class="btn btn-success">Cập nhật trạng thái</button>
-    </div>
-  </form>
-  <?php foreach ($newFeeds as $post) : ?>
-    <?php $userPost = findUserById($post['userId']); ?>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-12 mt-3">         
-          <div class="card">
-            <div class="card-horizontal">
-              <div class="img-square-wrapper">
-                <img class="rounded-circle" style="float: left;width:100px;height:100px;" src="<?php echo empty($userPost['avatarImage']) ? './assets/images/default-avatar.jpg' : 'view-image.php?userId=' . $post['userId'] ?>" alt="<?php echo $userPost['displayName'] ?>">
-              </div>
-              <div style="margin-left:100px;" class="card-body">
-                <h4 class="card-title"><?php echo $post['displayName']; ?>&nbsp;<img src='https://i.imgur.com/l63JR5Q.png' title=' Verified profile ' width='20' /></h4>
-                <small class="text-muted">Đăng lúc: <?php echo $post['createdAt']; ?></small>
-                <p class="card-text"><?php echo $post['content']; ?></p>
-                <?php if ($post['image'] != NULL) : ?>
-                  <figure>
-                    <img src="view-image.php?postId=<?php echo $post['id'] ?>" alt="<?php echo $post['id'] ?>" class="img-fluid">
-                  </figure>
-                <?php endif; ?>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  <?php endforeach; ?>  
-<?php else : ?>
-  <div class="jumbotron">
-    <section class="hero-section spad">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-xl-10 offset-xl-1">
-            <div class="row">
-              <div class="col-lg-7">
-                <div class="hero-info">
-                  <h5><strong>Yolo</strong> - Mạng xã hội dành cho sinh viên</h5>
-                  <div>
-                    <p>Miễn phí mà. Tham gia đi chờ chi...</p>
-                    <p>
-                      <a href="./register.php" class="btn btn-light" role="button">Đăng ký</a>
-                      <a href="./login.php" class="btn btn-light" role="button">Đăng nhập</a>
-                    </p>
-                  </div>
+    <?php endif; ?>
+    <div class="row">
+        <div class="col-md-12">
+            <form method="POST" enctype="multipart/form-data">
+                <div class="form-group">
+                    <textarea class="form-control" style="border-top-left-radius:0; border-top-right-radius: 0;"
+                        id="content" name="content" rows="3"
+                        placeholder="<?php echo $currentUser['displayName'] ?> ơi, bạn đang nghĩ gì vậy?"></textarea>
                 </div>
-              </div>
-              <div class="col-lg-5">
-                <figure class="hero-image">
-                  <img src="./assets/images/gif1.gif" alt="" class="img-fluid" alt="Responsive image">
-                </figure>
-              </div>
-            </div>
-          </div>
+                <div class="d-flex align-items-center">
+                    <div class="upload-btn-wrapper mr-2">
+                        <button class="btn">🖼️ <strong>Ảnh/Video</strong></button>
+                        <input type="file" id="postImage" name="postImage" />
+                    </div>
+                    <div class="form-group m-0">
+                        <div class="select-privacy">
+                            <select class="form-control">
+                                <option value="1">Công khai</option>
+                                <option value="2">Bạn bè</option>
+                                <option value="3">Chỉ mình tôi</option>
+                            </select>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-success ml-auto">Cập nhật trạng thái</button>
+                </div>
+            </form>
         </div>
-      </div>
-    </section>
-  </div>
-<?php endif; ?>
-<?php include 'footer.php' ?>
+    </div>
+    <?php foreach ($newFeeds as $post) : ?>
+    <?php $userPost = findUserById($post['userId']); ?>
+    <div class="row">
+        <div class="col-12 mt-3">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="d-flex">
+                        <div class="img-square-wrapper mr-2">
+                            <a href="#">
+                                <img class="rounded-circle" style="width:50px;height:50px;"
+                                    src="<?php echo empty($userPost['avatarImage']) ? './assets/images/default-avatar.jpg' : 'view-image.php?userId=' . $post['userId'] ?>"
+                                    alt="<?php echo $userPost['displayName'] ?>">
+                            </a>
+                        </div>
+                        <div>
+                            <a href="#" class="text-success">
+                                <h5 class="card-title mb-1"><?php echo $post['displayName']; ?>&nbsp;<img
+                                        src='https://i.imgur.com/l63JR5Q.png' title=' Verified profile ' width='20' />
+                                </h5>
+                            </a>
+                            <small class="text-muted">Đăng lúc:
+                                <?php echo $post['createdAt']; ?></small>
+                        </div>
+                    </div>
+                    <p class="card-text mt-3"><?php echo $post['content']; ?></p>
+                    <?php if ($post['image'] != NULL) : ?>
+                    <figure>
+                        <img src="view-image.php?postId=<?php echo $post['id'] ?>" alt="<?php echo $post['id'] ?>"
+                            class="img-fluid w-100">
+                    </figure>
+                    <?php endif; ?>
+                    <div class="d-flex justify-content-between">
+                        <div>
+                            <span>
+                                <i class="text-success fa fa-hand-o-right"></i>
+                                4 lượt thích
+                            </span>
+                        </div>
+                        <div>
+                            <span>13 bình luận</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer bg-transparent">
+                    <div class="d-flex react-group">
+                        <div class="hover-secondary w-100 text-center">
+                            <p id="btn-like" class="px-3 py-2"><i class="fa fa-hand-o-right"></i> Thích</p>
+                        </div>
+                        <div class="hover-secondary w-100 text-center">
+                            <p id="btn-comment" class="px-3 py-2"><i class="fa fa-comment"></i> Bình luận</p>
+                        </div>
+                    </div>
+                    <div class="comments mb-4">
+                        <div class="comment d-flex align-items-center mb-3">
+                            <a href="#">
+                                <img class="rounded-circle" style="width:40px;height:40px;"
+                                    src="<?php echo empty($userPost['avatarImage']) ? './assets/images/default-avatar.jpg' : 'view-image.php?userId=' . $post['userId'] ?>"
+                                    alt="<?php echo $userPost['displayName'] ?>">
+                            </a>
+                            <p class="rounded p-2 mb-0 ml-2" style="background-color: #eee;">
+                                <a href="#"
+                                    class="text-success font-weight-bold"><?php echo $currentUser['displayName'] ?></a>
+                                <span>asdijasd</span>
+                            </p>
+                        </div>
+                        <div class="comment d-flex align-items-center mb-3">
+                            <a href="#">
+                                <img class="rounded-circle" style="width:40px;height:40px;"
+                                    src="<?php echo empty($userPost['avatarImage']) ? './assets/images/default-avatar.jpg' : 'view-image.php?userId=' . $post['userId'] ?>"
+                                    alt="<?php echo $userPost['displayName'] ?>">
+                            </a>
+                            <p class="rounded p-2 mb-0 ml-2" style="background-color: #eee;">
+                                <a href="#"
+                                    class="text-success font-weight-bold"><?php echo $currentUser['displayName'] ?></a>
+                                <span>asdijasd</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="content-input">
+                        <div class="row">
+                            <div class="input-group mb-2">
+                                <input type="text" class="form-control" placeholder="Nhập bình luận ở đây...">
+                                <div class="input-group-append">
+                                    <button style="width: 80px;" class="btn btn-success" type="button">
+                                        <i class="fa fa-paper-plane"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php endforeach; ?>
+    <?php else : ?>
+    <div class="jumbotron">
+        <section class="hero-section spad">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xl-10 offset-xl-1">
+                        <div class="row">
+                            <div class="col-lg-7">
+                                <div class="hero-info">
+                                    <h5><strong>Yolo</strong> - Mạng xã hội dành cho sinh viên</h5>
+                                    <div>
+                                        <p>Miễn phí mà. Tham gia đi chờ chi...</p>
+                                        <p>
+                                            <a href="./register.php" class="btn btn-light" role="button">Đăng ký</a>
+                                            <a href="./login.php" class="btn btn-light" role="button">Đăng nhập</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-5">
+                                <figure class="hero-image">
+                                    <img src="./assets/images/gif1.gif" alt="" class="img-fluid" alt="Responsive image">
+                                </figure>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <?php endif; ?>
+    <?php include 'footer.php' ?>
