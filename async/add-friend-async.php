aea1da13cafc7a54595071cc32c00e5857cb05be
NDHau <nguyendinhhau1807@gmail.com>
2019-12-19 12:38:52 +0700

    Hau: make friend-request page async

---
+++ async/add-friend-async.php
@@ -0,0 +1,14 @@
+<?php
+require_once '../init.php';
+if (!$currentUser) {
+    header('Location: index.php');
+    exit();
+}
+
+if (isset($_POST['id'])) {
+    $user = findUserById($_POST['id']);
+    sendFriendRequest($currentUser['id'], $user['id']);
+    echo json_encode(array('success' => true));
+} else {
+    echo json_encode(array('success' => false));
+}
--- friend-request.php
+++ friend-request.php
@@ -21,8 +21,8 @@ $friendRequest = getFriendRequest($currentUser['id']);
                                 <?php echo '<img class="rounded-circle" style="width:50px;height:50px;" src="data:image/jpeg;base64,' . base64_encode($user['avatarImage']) . '"/>'; ?>
                                 <span class="pl-2 text-success"><?php echo $user['displayName'] ?></span>
                             </a>
-                            <div>
-                                <form action="./remove-friend.php" method="POST">
+                            <div data-id="<?php echo $user['id']; ?>">
+                                <form class="cancel-request" method="POST">
                                     <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
                                     <button type="submit" class="btn btn-success">Huỷ yêu cầu kết bạn</button>
                                 </form>
@@ -42,12 +42,12 @@ $friendRequest = getFriendRequest($currentUser['id']);
                                 <?php echo '<img class="rounded-circle" style="width:50px;height:50px;" src="data:image/jpeg;base64,' . base64_encode($user['avatarImage']) . '"/>'; ?>
                                 <span class="pl-2 text-success"><?php echo $user['displayName'] ?></span>
                             </a>
-                            <div>
-                                <form class="btn p-0" method="POST" action="add-friend.php">
+                            <div data-id="<?php echo $user['id']; ?>">
+                                <form class="accept-request btn p-0" method="POST">
                                     <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
                                     <button type="submit" class="btn btn-success">Xác nhận</button>
                                 </form>
-                                <form class="btn p-0" method="POST" action="remove-friend.php">
+                                <form class="remove-request btn p-0" method="POST">
                                     <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
                                     <button type="submit" class="btn btn-light">Xoá lời mời</button>
                                 </form>
@@ -58,5 +58,66 @@ $friendRequest = getFriendRequest($currentUser['id']);
             <?php endforeach; ?>
     </div>
 </div>
+
+<script type="text/javascript">
+    $(document).ready(_ => {
+        $('.cancel-request').submit(function(e) {
+            e.preventDefault();
+            $.ajax({
+                type: 'POST',
+                url: './async/remove-friend-async.php',
+                data: $(this).serialize(),
+                success: (response) => {
+                    var jsonData = JSON.parse(response);
+                    if (jsonData.success == true) {
+                        let div = $(this).parent();
+                        $(this).remove();
+                        div.append($(`<a href="./profile.php?id=${div.data('id')}" class="btn btn-light">Xem thông tin</a>`));
+                    } else {
+                        alert('Có lỗi xảy ra!');
+                    }
+                }
+            });
+        });
+
+        $('.remove-request').submit(function(e) {
+            e.preventDefault();
+            $.ajax({
+                type: 'POST',
+                url: './async/remove-friend-async.php',
+                data: $(this).serialize(),
+                success: (response) => {
+                    var jsonData = JSON.parse(response);
+                    if (jsonData.success == true) {
+                        let div = $(this).parent();
+                        div.empty();
+                        div.append($(`<a href="./profile.php?id=${div.data('id')}" class="btn btn-light">Xem thông tin</a>`));
+                    } else {
+                        alert('Có lỗi xảy ra!');
+                    }
+                }
+            });
+        });
+
+        $('.accept-request').submit(function(e) {
+            e.preventDefault();
+            $.ajax({
+                type: 'POST',
+                url: './async/add-friend-async.php',
+                data: $(this).serialize(),
+                success: (response) => {
+                    var jsonData = JSON.parse(response);
+                    if (jsonData.success == true) {
+                        let div = $(this).parent();
+                        div.empty();
+                        div.append($(`<a href="./profile.php?id=${div.data('id')}" class="btn btn-success">Bạn bè</a>`));
+                    } else {
+                        alert('Có lỗi xảy ra!');
+                    }
+                }
+            });
+        });
+    });
+</script>
 
 <?php include 'footer.php' ?>
