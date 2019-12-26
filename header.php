36bac34b985928f1cb4260af3e29977324c91b98
nttanh6299 <nttanh6299@gmail.com>
2019-12-26 17:04:19 +0700

    TA: add search box, delete message button

--- header.php
+++ header.php
@@ -6,9 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>LTWEB1 - BTN</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css"
+        integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/style.css">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://js.pusher.com/5.0/pusher.min.js"></script>
@@ -22,30 +24,50 @@
                 <a href="./" class="navbar-brand text-light">
                     <h1 style="font-weight: 700;">Yolo</h1>
                 </a>
-                <div>
-                    <?php if ($currentUser) : ?>
-                        <div class="d-flex justify-content-center align-items-center ml-auto">
-                            <a data-toggle="tooltip" title="Thông tin cá nhân" href="./profile.php?id=<?php echo $currentUser['id']; ?>" class="btn btn-success d-flex justify-content-center align-items-center">
-                                <i style="margin-right: 6px;" class="fa fa-user"></i>
-                                <?php echo $currentUser['displayName']; ?>
-                            </a>
-                            <span class="divider"></span>
-                            <a href="./" class="btn btn-success">Trang chủ</a>
-                            <span class="divider"></span>
-                            <div class="icon-group">
-                                <a class="icon" data-toggle="tooltip" href="./friend-request.php" title="Lời mời kết bạn">
-                                    <i class="fa fa-users">
-                                    </i>
-                                </a>
-                                <a class="icon" data-toggle="tooltip" href="./messages.php" title="Tin nhắn">
-                                    <i class="fa fa-comment">
-                                    </i>
-                                </a>
-                                <a class="icon" href="./logout.php" data-toggle="tooltip" title="Đăng xuất">
-                                    <i class="fa fa-sign-out"></i>
-                                </a>
+                <!-- search box -->
+                <div class="search-box">
+                    <form>
+                        <div class="content-input">
+                            <div class="row">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Tìm kiếm bạn bè..." />
+                                    <div class="input-group-append">
+                                        <button class="btn btn-light" type="submit">
+                                            <i class="fa fa-search"></i>
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
+                    </form>
+                </div>
+                <!-- /search box -->
+                <div>
+                    <?php if ($currentUser) : ?>
+                    <div class="d-flex justify-content-center align-items-center ml-auto">
+                        <a data-toggle="tooltip" title="Thông tin cá nhân"
+                            href="./profile.php?id=<?php echo $currentUser['id']; ?>"
+                            class="btn btn-success d-flex justify-content-center align-items-center">
+                            <i style="margin-right: 6px;" class="fa fa-user"></i>
+                            <?php echo $currentUser['displayName']; ?>
+                        </a>
+                        <span class="divider"></span>
+                        <a href="./" class="btn btn-success">Trang chủ</a>
+                        <span class="divider"></span>
+                        <div class="icon-group">
+                            <a class="icon" data-toggle="tooltip" href="./friend-request.php" title="Lời mời kết bạn">
+                                <i class="fa fa-users">
+                                </i>
+                            </a>
+                            <a class="icon" data-toggle="tooltip" href="./messages.php" title="Tin nhắn">
+                                <i class="fa fa-comment">
+                                </i>
+                            </a>
+                            <a class="icon" href="./logout.php" data-toggle="tooltip" title="Đăng xuất">
+                                <i class="fa fa-sign-out"></i>
+                            </a>
+                        </div>
+                    </div>
                     <?php endif; ?>
                 </div>
             </div>
